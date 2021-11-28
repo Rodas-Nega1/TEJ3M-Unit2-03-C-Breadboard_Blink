b9f273954cad69e2428990691c866b2e5fa69936
Rodas-Nega1 <90275822+Rodas-Nega1@users.noreply.github.com>
2021-11-28 11:53:30 -0500

    Create breadboard_blink.c

---
+++ breadboard_blink.c
@@ -0,0 +1,21 @@
+/* 
+  Created On: Nov 2021
+  Created By: Rodas Nega
+ 
+  Turns on LED on for one second, then off for one second, repeatedly,
+  but the interval increases each time by 1 more second
+*/
+
+int blinkTime = 1000; // set variable to 1000
+
+void setup() {
+  // initialize digital pin 13 as an output
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(blinkTime);       // wait for length of variable blinkTime
+  digitalWrite(13, LOW);  // turn the LED off by making the voltage LOW
+  delay(1000);            // wait for a second
+}
